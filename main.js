de03b60477967d0db53a4622374f3dc35b5c93e7
meflems <89581469+meflems@users.noreply.github.com>
2022-03-12 20:22:12 +0700

    update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 localStorage.setItem("mgo", 0);
 var mgo = 0;
+var encrypted = CryptoJS.AES.encrypt("CoinEnc", localStorage.getItem("coins"));
 if(!localStorage.getItem("visited")){
 	localStorage.setItem("coins", 0);
 	localStorage.setItem("visited", "yes");
