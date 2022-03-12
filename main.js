61da04c5273796abd7a45a2fdab3e4e844aaa6c6
meflems <89581469+meflems@users.noreply.github.com>
2022-03-12 09:25:39 +0700

    update main.js

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ function punchClick() {
 	var coinEarn = Math.round(Math.random()*1)+9;
 	var coinEarnsuc = coin += coinEarn
 	console.log(coin);
-	localStorage.setItem("coins", coin);
+	localStorage.setItem("yes", coin);
     document.getElementById("coins").innerHTML = yes;
 }
 
